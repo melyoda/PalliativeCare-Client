60569e80f3bb8db9077caf41a161e871710b71ac
melyoda <amv123doom@gmail.com>
2025-09-25 12:37:00 +0300

    Deleted faulty backup

--- palliatave_care_client - BACKUP/android/app/src/main/kotlin/com/example/palliatave_care_client/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.palliatave_care_client
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- palliatave_care_client - BACKUP/lib/models/user_type.dart
+++
@@ -1,2 +0,0 @@
-// Enum for user type - often in a shared `enums.dart` or `models.dart` file
-enum UserType { patient, doctor }
